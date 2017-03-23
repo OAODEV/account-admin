ca8ba3612f72a6dba643f01b68101bc107299535
Thomas Yager-Madden <thomas.yager-madden@adops.com>
2017-03-23 15:51:02 -0500

    Mission clean forms (#8)
    * Changed ORM to SQLAlchemy for better m2m handling
    Added properties to ModelView classes in the app to apply
    more user-friendly form labels, and straighten out list sorting
    Addresses issues #1 - 7 here:
    https://github.com/OAODEV/account-admin/issues
    
    * Make SECRET_KEY actually secret
    
    * Add circle.yml
    
    * Add make_secret_function to set SECRET_KEY default in admin_app

--- Dockerfile
+++ Dockerfile
@@ -9,10 +9,12 @@ postgresql-dev
 
 COPY . /app
 WORKDIR /app
-
+RUN echo "0 5 * * * python3 /app/google_directory_sync.py" > /tmp/crontab \
+    && crontab /tmp/crontab
 
 RUN pip install -r requirements.txt
 
 EXPOSE 5000
 
+# CMD crond -d 5; python3 admin_app.py
 CMD python3 admin_app.py
--- google_directory_sync.py
+++ google_directory_sync.py
@@ -8,13 +8,14 @@ from apiclient import discovery
 from oauth2client import client, tools
 from oauth2client.file import Storage
 
-from playhouse.db_url import connect
-from models import Person
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from models import Employee
 
-DBURL = os.getenv('DBURL', ('postgres://account_admin_user@localhost'
-                            '/account_admin?sslmode=verify-ca'))
-
-database = connect(DBURL)
+engine = create_engine(
+    os.getenv('DBURL', ('postgres://account_admin_user@localhost:5433'
+                        '/account_admin?sslmode=verify-ca')))
+Session = sessionmaker(bind=engine)
 
 
 SCOPES = 'https://www.googleapis.com/auth/admin.directory.user'
@@ -50,7 +51,15 @@ def get_credentials():
 
 def main():
     """
+    Simple script to fetch all OAO internal users from Google Directory API
+    and sync to the 'person' table in our 'account_admin' database.
 
+    Users are matched based on Google user id. Any id values appearing in the
+    API pull that aren't already added get a row inserted with name, gsuite_id,
+    and email. Manager is left to be filled in by the administrative user.
+
+    After that, and similiarly, any id values in the database table that are
+    not in the pull from Google have their current_employee_flag set to False
     """
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
@@ -59,27 +68,38 @@ def main():
     # Get all Google Users from Directory API
     results = service.users().list(
         customer='my_customer',
-        projection='full',
         query="orgUnitPath='/Google Users'",
         orderBy='email').execute()
     users = results.get('users', [])
+    user_ids = [user['id'] for user in users]
 
-    # Get list of person codes (== user['id'] from google) from database
-    person_query = Person.select()
-    person_codes = []
-    for person in person_query:
-        person_codes.append(person.person_code)
+    # Get list of Employee codes (== user['id'] from google) from database
+    session = Session()
+    person_query = session.query(Employee).all()
+    person_codes = [person.gsuite_id for person in person_query]
 
     # Add any Google users to 'person' table if user['id'] not already
     # in person_code
     for user in users:
         if user['id'] not in person_codes:
             print(user['primaryEmail'], user['id'])
-            q = Person.insert(first_name=user['name']['givenName'],
-                              last_name=user['name']['familyName'],
-                              person_code=user['id'],
-                              email=user['primaryEmail'])
-            q.execute()
+            q = Employee(first_name=user['name']['givenName'],
+                         last_name=user['name']['familyName'],
+                         gsuite_id=user['id'],
+                         email=user['primaryEmail'])
+            session.add(q)
+            session.commit()
+
+    # Set current_employee_flag to False if person's gsuite_id not found in
+    # list of ids from Google, as a way to "soft delete" these users
+
+    # TODO: This will keep updating users that have already been soft-deleted.
+    #       Not a _terrible_ issue, but we should do better.
+    for person in person_codes:
+        if person not in user_ids:
+            q = session.query(Employee).filter_by(gsuite_id=person).first()
+            q.current_employee_flag = False
+            session.commit()
 
 if __name__ == '__main__':
     main()
