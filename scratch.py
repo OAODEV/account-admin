636107ab9f550973a45c4bc15a88ff74de488f57
Thomas Yager-Madden <10351894+tym-oao@users.noreply.github.com>
2018-07-23 16:04:08 -0500

    Update Pipfile

--- scratch.py
+++
@@ -1,8 +0,0 @@
-class MyHomeView(AdminIndexView):
-    @expose('/')
-    def index(self):
-        arg1 = 'Hello'
-        return self.render('admin/myhome.html', arg1=arg1)
-
-admin = Admin(index_view=MyHomeView())
-
